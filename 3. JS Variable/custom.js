ee6d17f15d354da36072b9cd0182528dd12e9b95
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-10-09 22:54:43 +0600

    create custom.js

---
+++ 3. JS Variable/custom.js
@@ -0,0 +1,19 @@
+// Automatically
+// Using var
+// Using let
+// Using const
+
+// The let and const keywords were added to JavaScript in 2015.
+// The var keyword should only be used in code written for older browsers.
+// JavaScript identifiers are case-sensitive.
+// It's a good programming practice to declare all variables at the beginning of a script.
+
+"use strict";
+
+let strings = "Hi, I am learning JS in 2024";
+alert(strings);
+
+// Const value only one time assign and never be changed
+const pi = 3.1416; 
+// pi = pi + 20; it's not working
+alert(pi);
